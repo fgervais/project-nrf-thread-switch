10e190a1b2f9b1927d9f0c39a732501c6b33ea5a
Francois Gervais <francoisgervais@gmail.com>
2023-09-30 11:00:35 -0400

    Adjust main file

--- app/src/main.c
+++ app/src/main.c
@@ -13,9 +13,41 @@ LOG_MODULE_REGISTER(main, LOG_LEVEL_DBG);
 #include "openthread.h"
 
 
+static struct ha_switch temperature_sensor = {
+	.name = "Switch",
+	.device_class = "switch",
+};
+
+
 int main(void)
 {
+	const struct device *wdt = DEVICE_DT_GET(DT_NODELABEL(wdt0));
 	const struct device *cons = DEVICE_DT_GET(DT_CHOSEN(zephyr_console));
+
+	int main_wdt_chan_id = -1, mqtt_wdt_chan_id = -1;
+	uint32_t reset_cause;
+
+
+	init_watchdog(wdt, &main_wdt_chan_id, &mqtt_wdt_chan_id);
+
+	LOG_INF("\n\n🚀 MAIN START (%s) 🚀\n", APP_VERSION_FULL);
+
+	reset_cause = show_reset_cause();
+	clear_reset_cause();
+
+	if (is_reset_cause_watchdog(reset_cause)
+	    || is_reset_cause_button(reset_cause)) {
+		ret = openthread_erase_persistent_info();
+		if (ret < 0) {
+			LOG_WRN("Could not erase openthread info");
+		}
+	}
+
+	ret = openthread_my_start();
+	if (ret < 0) {
+		LOG_ERR("Could not start openthread");
+		return ret;
+	}
 
 	if (app_event_manager_init()) {
 		LOG_ERR("Event manager not initialized");
@@ -23,14 +55,11 @@ int main(void)
 		module_set_state(MODULE_STATE_READY);
 	}
 
-	openthread_enable_ready_flag();
+	LOG_INF("💤 waiting for openthread to be ready");
+	openthread_wait_for_ready();
 
-	while (!openthread_ready)
-		k_sleep(K_MSEC(100));
-	// openthread_set_csl_period_ms(1000);
-
-	// Something else is not ready, not sure what
-	k_sleep(K_MSEC(100));
+	mqtt_watchdog_init(wdt, mqtt_wdt_chan_id);
+	ha_start(uid_get_device_id());
 
 	while (1) {
 		dns_resolve_finished = false;
