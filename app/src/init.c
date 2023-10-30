4122d51c05fa2ceba46813f6600273632d91692d
Francois Gervais <francoisgervais@gmail.com>
2023-10-30 12:05:13 -0400

    Use CONFIGs for remaining delays

--- app/src/init.c
+++ app/src/init.c
@@ -7,16 +7,13 @@ LOG_MODULE_REGISTER(init, LOG_LEVEL_DBG);
 #include "init.h"
 
 
-#define WATCHDOG_TIMEOUT_SEC	(12 * 60 + 40)
-
-
 static int watchdog_new_channel(const struct device *wdt)
 {
 	int ret;
 
 	struct wdt_timeout_cfg wdt_config = {
 		.window.min = 0,
-		.window.max = WATCHDOG_TIMEOUT_SEC * MSEC_PER_SEC,
+		.window.max = CONFIG_APP_WATCHDOG_TIMEOUT_SEC * MSEC_PER_SEC,
 		.callback = NULL,
 		.flags = WDT_FLAG_RESET_SOC,
 	};
